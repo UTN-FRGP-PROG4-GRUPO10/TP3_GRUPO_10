dc6b44af48619832496fd0be7b56afc63bdf55c4
Tomás Martínez <tomas.emart@gmail.com>
2025-09-17 01:20:40 -0300

    feat-v5: add DaoProducto.obtenerProducto() and obtenerTodosLosProductos()

--- src/dao/DaoProducto.java
+++ src/dao/DaoProducto.java
@@ -2,7 +2,9 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 import entidad.Producto;
 
@@ -35,5 +37,60 @@ public class DaoProducto {
 		}
 		
 		return filas;
+	}
+	
+	public Producto obtenerProducto(String codigo) {
+		
+		Producto producto = new Producto();
+		Connection cn = null;
+		
+		try {
+			cn = DriverManager.getConnection(host + dbName, user, pass);
+			Statement st = cn.createStatement();
+			String query = "SELECT * FROM productos WHERE Codigo = '" + codigo + "'";
+			ResultSet rs = st.executeQuery(query);
+			
+			while(rs.next()) {
+				producto.setCodigo(codigo);
+				producto.setNombre(rs.getString("Nombre"));	
+				producto.setPrecio(rs.getDouble("Precio"));	
+				producto.setStock(rs.getInt("Stock"));
+				producto.setIdCategoria(rs.getInt("IdCategoria"));				
+			}
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return producto;
+	}
+	
+	public ArrayList<Producto> obtenerTodosLosProductos() {
+		ArrayList<Producto> productos = new ArrayList<Producto>();
+		
+		Connection cn = null;
+		try {
+			cn = DriverManager.getConnection(host + dbName, user, pass);
+			Statement st = cn.createStatement();
+			String query = "SELECT * FROM productos";
+			ResultSet rs = st.executeQuery(query);
+			
+			while(rs.next()) {
+				Producto producto = new Producto();
+				producto.setCodigo(rs.getString("Codigo"));
+				producto.setNombre(rs.getString("Nombre"));	
+				producto.setPrecio(rs.getDouble("Precio"));	
+				producto.setStock(rs.getInt("Stock"));
+				producto.setIdCategoria(rs.getInt("IdCategoria"));
+				
+				productos.add(producto);
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return productos;
 	}
 }
--- src/main/Principal.java
+++ src/main/Principal.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.util.ArrayList;
+
 import dao.DaoCategoria;
 import dao.DaoProducto;
 import entidad.Categoria;
@@ -10,30 +12,41 @@ public class Principal {
 	
 		public static void main(String[] args) {
 			DaoProducto productoDao = new DaoProducto();
-			DaoCategoria categoriaDao = new DaoCategoria();
+//			DaoCategoria categoriaDao = new DaoCategoria();
+//			
+//			Categoria categoria1 = new Categoria();
+//			categoria1.setNombre("lacteos");
+//			int filas = categoriaDao.agregarCategoria(categoria1);
 			
-			Categoria categoria1 = new Categoria();
-			categoria1.setNombre("lacteos");
-			int filas = categoriaDao.agregarCategoria(categoria1);
+			Producto producto = productoDao.obtenerProducto("1");
+			System.out.println(producto.toString());
 			
-			if(filas==1)
-					System.out.println("Categoria agregada");
-			else
-				System.out.println("Categoria no agregada");
+			ArrayList<Producto> productos = productoDao.obtenerTodosLosProductos();
+			System.out.println("LISTADO DE PRODUCTOS:");
+			for (Producto p : productos) {
+				System.out.println(p.toString());
+			}
 			
 			
-			Producto producto1 = new Producto();
-			producto1.setCodigo("1");
-			producto1.setNombre("leche");
-			producto1.setPrecio(3000);
-			producto1.setStock(10);
-			producto1.setIdCategoria(1);
-			
-			filas = productoDao.agregarProducto(producto1);
-			
-			if(filas==1)
-					System.out.println("Producto agregado");
-			else
-				System.out.println("Producto no agregado");
+//			
+//			if(filas==1)
+//					System.out.println("Categoria agregada");
+//			else
+//				System.out.println("Categoria no agregada");
+//			
+//			
+//			Producto producto1 = new Producto();
+//			producto1.setCodigo("1");
+//			producto1.setNombre("leche");
+//			producto1.setPrecio(3000);
+//			producto1.setStock(10);
+//			producto1.setIdCategoria(1);
+//			
+//			filas = productoDao.agregarProducto(producto1);
+//			
+//			if(filas==1)
+//					System.out.println("Producto agregado");
+//			else
+//				System.out.println("Producto no agregado");
 		}
 }
